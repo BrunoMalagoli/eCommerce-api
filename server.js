30b097e8ad38fb075a07f3ed2b0bf5d5b612cfd6
Bruno Malagoli <bmalagoli2@gmail.com>
2022-04-06 17:29:58 -0300

    Creando carpetas y routers

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const PORT = 8080;
+const productosRouter = require("./routes/productosRouter")
+const carritoRouter = require("./routes/carritoRouter")
+//configs
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+//Routes
+app.use(express.static(__dirname + "./public"))
+app.use("/api/productos", productosRouter)
+app.use("/api/carrito", carritoRouter)
+
+const server = app.listen(PORT, ()=>{
+    console.log(`Servidor escuchando en ${PORT}`)
+})
+server.on("error", (error)=>{
+    console.log(error.message)
+})
+
+app.get("/" ,(req, res)=>{
+    res.sendFile(__dirname + "/public/views/index.html")
+})
