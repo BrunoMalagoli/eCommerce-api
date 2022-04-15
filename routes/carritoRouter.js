329e9734ee240996246a763e59fd88194d1edfd7
Bruno Malagoli <bmalagoli2@gmail.com>
2022-04-15 01:55:52 -0300

    Agregando getById

--- routes/carritoRouter.js
+++ routes/carritoRouter.js
@@ -27,15 +27,54 @@ class Carrito{
         }
         return carrito.id
     }
+    async deleteById(id){
+        try{
+            let data = await fs.promises.readFile("./storage/carritos.txt", "utf-8");
+            data = await JSON.parse(data);
+            await data.splice(id, 1 , "deleted");
+            await fs.promises.writeFile("./storage/carritos.txt", JSON.stringify(data))
+            console.log(data)
+            return data
+        }
+        catch(err){
+            console.log(err)
+        }
+    }
+    async getById(id){
+        try{
+            let data = await fs.promises.readFile("./storage/carritos.txt", "utf-8");
+            console.log(data)
+            data = await JSON.parse(data);
+            const dataFiltered = await data.filter((carrito)=> carrito.id === id)
+            console.log(dataFiltered)
+            let productos = await dataFiltered.map(carrito => {
+                console.log(carrito.productos)
+                return carrito.productos;
+            });
+            console.log(await productos)
+            return productos
+        }catch(err){
+            console.log(err)
+        }
+    }
 }
 //Instancias
 const carr = new Carrito();
-let adminLogged
-carr.createCarrito();
-carr.createCarrito({"camisa":"del barcita"})
+//carr.createCarrito();
+//carr.createCarrito({"productos":" camiseta del barcita"})
 //Endpoints
 router.get("/", (req,res)=>{
     res.send("Aqui esta el carrito")
 })
-
+router.post("/", async (req, res)=>{
+    res.json({"Id del carrito": await carr.createCarrito({"camisa":"del barcita"})})
+})
+router.delete("/:id", async (req,res)=>{
+    let id = req.params.id;
+    res.json({"Carrito eliminado, contenido actual": await carr.deleteById(id)})
+})
+router.get("/:id/productos", async (req,res)=>{
+    let id = parseInt(req.params.id);
+    res.json({"Tus productos" : await carr.getById(id)})
+})
 module.exports = router;
